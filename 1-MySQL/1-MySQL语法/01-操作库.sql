f94986d3f8e1fec1e143abb39fd9536eb123ba21
蓝田_Loto <shorfng@126.com>
2021-06-17 22:46:19 +0800

    更改数据库名称为小写（mac系统下区分数据库名的大小写）

--- 1-MySQL/1-MySQL语法/01-操作库.sql
+++ 1-MySQL/1-MySQL语法/01-操作库.sql
@@ -1,14 +1,14 @@
 # 创建数据库
 # create database 数据库名;
-create database if not exists `TD_MySQL_DB` default character set utf8mb4 collate utf8mb4_unicode_ci;
+create database if not exists `td_mysql_db` default character set utf8mb4 collate utf8mb4_unicode_ci;
 
 # 选择数据库
 # use 数据库名;
-use TD_MySQL_DB;
+use td_mysql_db;
 
 # 删除数据库
 # drop database 数据库名;
-drop database TD_MySQL_DB;
+drop database td_mysql_db;
 
 
 
