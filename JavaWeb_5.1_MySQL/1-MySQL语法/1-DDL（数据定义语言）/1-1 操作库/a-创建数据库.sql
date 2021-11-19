4437f423cfd23d2f5dc4c7f31b17d9b2387eeb8c
蓝田_Loto <shorfng@126.com>
2021-11-19 19:42:11 +0800

    修改数据库名称

--- JavaWeb_5.1_MySQL/1-MySQL语法/1-DDL（数据定义语言）/1-1 操作库/a-创建数据库.sql
+++ JavaWeb_5.1_MySQL/1-MySQL语法/1-DDL（数据定义语言）/1-1 操作库/a-创建数据库.sql
@@ -1,10 +1,10 @@
 # 创建数据库
 # create database 数据库名;
-create database if not exists `td_mysql_db` default character set utf8mb4 collate utf8mb4_unicode_ci;
+create database if not exists `JavaWeb_5.1_MySQL` default character set utf8mb4 collate utf8mb4_unicode_ci;
 
 # -----------------------------------------------------------------
 
 # 选择数据库/切换数据库
 # use 数据库名;
-use td_mysql_db;
+use `JavaWeb_5.1_MySQL`;
 
