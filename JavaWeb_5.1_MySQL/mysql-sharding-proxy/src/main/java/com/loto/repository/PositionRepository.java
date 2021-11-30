3bff680c44a0763bb308bbf3a499bab8f3f11cc0
蓝田_Loto <shorfng@126.com>
2021-11-30 20:47:56 +0800

    mysql-Sharding-Proxy

---
+++ JavaWeb_5.1_MySQL/mysql-sharding-proxy/src/main/java/com/loto/repository/PositionRepository.java
@@ -0,0 +1,7 @@
+package com.loto.repository;
+
+import com.loto.entity.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PositionRepository extends JpaRepository<Position, Long> {
+}
