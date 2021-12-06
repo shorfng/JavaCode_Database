ca058e968fed1d0e2db86e901ac3db3f61e5c87d
蓝田_Loto <shorfng@126.com>
2021-12-06 21:29:03 +0800

    MongoDB 使用（SpringBoot 项目 - MongoRepository）

---
+++ JavaWeb_5.4_MongoDB/mongodb4-springboot-mongorepository/src/main/java/com/loto/repository/ResumeRepository.java
@@ -0,0 +1,12 @@
+package com.loto.repository;
+
+import com.loto.bean.Resume;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ResumeRepository extends MongoRepository<Resume, String> {
+    List<Resume> findByNameEquals(String name);
+
+    List<Resume> findByNameAndExpectSalary(String name, double expectSalary);
+}
